632699d1267d07c6f78a451466efdb71ef9af0a1
Yevhen Kornilov <zhenyakornilov98@gmnail.com>
2026-03-15 19:43:46 +0200

    Calculate error percentage

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,13 @@ void loop()
   float voltageMv = analogReadMilliVolts(ADC_PIN);
   float voltageMvtoV = voltageMv / 1000;
   float diff = voltageMvtoV - calcVoltage;
+  float errorPercent = (diff / voltageMvtoV) * 100;
+
   Serial.println("RAW ADC Value: " + String(rawValue));
   Serial.println("Calculated voltage: " + String(calcVoltage));
   Serial.println("Calculated voltage with calibration: " + String(voltageMv));
   Serial.println("Difference in millivolts: " + String(diff * 1000));
+  Serial.println("Difference in percent: " + String(errorPercent) + String("%"));
   Serial.println("==============================");
   delay(1000);
 }
