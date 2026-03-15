37b8a41b4977c281be19d2ecee5b24a5b4850218
Yevhen Kornilov <zhenyakornilov98@gmnail.com>
2026-03-15 20:03:06 +0200

    Add ADC resolution and attenuation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 
 #define ADC_PIN 4
-#define ADC_MAX 4095
+#define ADC_BITS 12
+#define ADC_MAX (pow(2, ADC_BITS) - 1)
 
 void setup()
 {
   Serial.begin(115200);
+  analogReadResolution(ADC_BITS);
+  analogSetPinAttenuation(ADC_PIN, ADC_11db);
 }
 
 void loop()
@@ -15,8 +18,15 @@ void loop()
   float voltageMv = analogReadMilliVolts(ADC_PIN);
   float voltageMvtoV = voltageMv / 1000;
   float diff = voltageMvtoV - calcVoltage;
+
+  if (diff < 0)
+  {
+    diff = diff * -1;
+  }
+
   float errorPercent = (diff / voltageMvtoV) * 100;
 
+  Serial.println("ADC MAX: " + String(ADC_MAX));
   Serial.println("RAW ADC Value: " + String(rawValue));
   Serial.println("Calculated voltage: " + String(calcVoltage));
   Serial.println("Calculated voltage with calibration: " + String(voltageMv));
